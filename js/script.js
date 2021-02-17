bfcbc69973ee904e491868d364edb2352389aa82
Artisans Alley <58870603+ArtisansAlley@users.noreply.github.com>
2021-02-17 15:04:21 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var width = $(window).width();
+var height =  $(window).height();
+
+
+function set_up_cam() {
+    Webcam.set('constraints',{
+        video: true,
+        facingMode: "environment"
+        });
+        Webcam.set({
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+    Webcam.attach('#camera');
+}
+
+Webcam.on( 'live', function() {    
+    setTimeout(function() {
+        $('#welcome').fadeOut();
+        $('#capture').show();
+    }, 1000)
+} );
+
+function take_snapshot() {
+    Webcam.freeze()
+    Webcam.snap( function(data_uri) {
+        data = {'img': data_uri};
+        data = JSON.stringify(data);
+        $.ajax({
+        url: 'http://manifestocrafters.mine.bz/upload',
+        method: 'POST',
+        contentType: "application/json",
+        dataType: "json",
+        data: data,
+        complete:function(res) {
+        alert(res.responseJSON.message);
+        }
+        })
+        // Webcam.upload( data_uri, , function(code, text) {
+        //   console.log(text);
+        // });
+    });
+}
+
+$(document).ready(function() {
+    set_up_cam();
+})
